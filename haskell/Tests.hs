077cf8e4cb9f9affa206fdeecc44d3e751d5850d
Aliena Ziegler <aliena.c.ziegler@gmail.com>
2023-09-23 15:57:11 +0200

    added tests CPS

--- haskell/Tests.hs
+++ haskell/Tests.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 import Control.Monad.Cont
 
 main :: IO ()
-main = testNonCPS
+main = testMonadicCPS
 
 
 testNonCPS :: IO ()
@@ -74,22 +74,90 @@ testNonCPS = do
 
 testCPS :: IO ()
 testCPS = do
-    putStrLn "-------- Constant evaluation: ----------------------------"
-    print (CPS.eval (CPS.Const 5) [] id)
-    putStrLn "-------- Variable evaluation: ----------------------------"
-    putStrLn "-------- Addition evaluation: ----------------------------"
-    putStrLn "-------- Function evaluation: ----------------------------"
-    putStrLn "-------- Function application evaluation: ----------------"
-    putStrLn "-------- Object evaluation: ------------------------------"
-    putStrLn "-------- Object field evaluation: ------------------------"
+    putStrLn "\n-------- Constant evaluation: ----------------------------"
+    putStr $ "5 = " ++ show (CPS.eval (CPS.Const 5) [] id)
+    
+    putStrLn "\n-------- Variable evaluation: ----------------------------"
+    putStrLn "Let [(x = 3), (y = 7)]"
+    putStr $ "x = " ++ show (CPS.eval (CPS.Var "x") [("x", CPS.NumVal 3), ("y", CPS.NumVal 7)] id)
+    
+    putStrLn "\n-------- Addition evaluation: ----------------------------"
+    putStrLn "Let [(x = 3), (y = 5)]"
+    putStr "3 + y = "
+    print $ CPS.eval (CPS.Add (CPS.Const 3) (CPS.Var "y")) [("x", CPS.NumVal 3), ("y", CPS.NumVal 5)] id
+    putStr "x + 5 = "
+    print $ CPS.eval (CPS.Add (CPS.Var "x") (CPS.Const 5)) [("x", CPS.NumVal 3), ("y", CPS.NumVal 5)] id
+    putStr "x + y = "
+    print $ CPS.eval (CPS.Add (CPS.Var "x") (CPS.Var "y")) [("x", CPS.NumVal 3), ("y", CPS.NumVal 5)] id
+    
+    putStrLn "\n-------- Function evaluation: ----------------------------"
+    let f = CPS.Fun ["x", "y"] (CPS.Add (CPS.Var "x") (CPS.Var "y")) 
+    putStr $ "f(x,y) = " ++ show (CPS.eval f [] id) 
+
+    putStrLn "now let [(x = 7)]"
+    putStr $ "f(x,y) = " ++ show (CPS.eval f [("x", CPS.NumVal 7)] id)
+    
+    putStrLn "\n-------- Function application evaluation: ----------------"
+    putStr "f(x=1,y=2) = "
+    print $ CPS.eval (CPS.App f [CPS.Const 1, CPS.Const 2]) [] id
+
+    putStrLn "now let [(x = 7)]"
+    putStr "f(x=1,y=2) = "
+    print $ CPS.eval (CPS.App f [CPS.Const 1, CPS.Const 2]) [("x", CPS.NumVal 7)] id
+
+    putStrLn "\n-------- Object evaluation: ------------------------------"
+    let obj1 = CPS.Obj [("field1", CPS.Const 42), ("field2", CPS.Const 99)]
+    putStr "obj1 = "
+    print $ CPS.eval obj1 [] id 
+
+    putStrLn " now let [(x = 5)]"
+    let obj2 = CPS.Obj [("field1", CPS.Var "x"), ("field2", f)]
+    putStr "obj2 = "
+    print $ CPS.eval obj2 [("x", CPS.NumVal 5)] id
+
+    putStrLn "\n-------- Object field evaluation: ------------------------"
+    putStrLn "let [(obj1 = ...), (obj2 = ...)]"
+    let env = [("obj1", CPS.eval obj1 [] id), ("obj2", CPS.eval obj2 [("x", CPS.NumVal 5)] id)]
+    let field11 = CPS.Field (CPS.Var "obj1") "field1"
+    let field12 = CPS.Field (CPS.Var "obj1") "field2"
+    let field21 = CPS.Field (CPS.Var "obj2") "field1"
+    let field22 = CPS.Field (CPS.Var "obj2") "field2"
+    putStrLn $ "object1 field1: " ++ show (CPS.eval field11 env id)
+    putStrLn $ "object1 field2: " ++ show (CPS.eval field12 env id)
+    putStrLn $ "object2 field1: " ++ show (CPS.eval field21 env id)
+    putStrLn $ "object2 field2: " ++ show (CPS.eval field22 env id)
 
 
-testMonadicCPS :: IO ()
-testMonadicCPS = do
-    putStrLn "-------- Constant evaluation: ----------------------------"
-    putStrLn "-------- Variable evaluation: ----------------------------"
-    putStrLn "-------- Addition evaluation: ----------------------------"
-    putStrLn "-------- Function evaluation: ----------------------------"
-    putStrLn "-------- Function application evaluation: ----------------"
-    putStrLn "-------- Object evaluation: ------------------------------"
-    putStrLn "-------- Object field evaluation: ------------------------"
+-- testMonadicCPS :: IO ()
+-- testMonadicCPS = do
+--     putStrLn "\n-------- Constant evaluation: ----------------------------"
+--     putStr $ "5 = " ++ show (runCont $ Monadic.eval (Monadic.Const 5) [])
+    
+--     putStrLn "\n-------- Variable evaluation: ----------------------------"
+--     putStrLn "Let [(x = 3), (y = 7)]"
+--     putStr $ "x = " ++ show (Monadic.eval (Monadic.Var "x") [("x", Monadic.NumVal 3), ("y", Monadic.NumVal 7)])
+    
+--     putStrLn "\n-------- Addition evaluation: ----------------------------"
+--     putStrLn "Let [(x = 3), (y = 5)]"
+--     putStr "3 + y = "
+--     print $ Monadic.eval (Monadic.Add (Monadic.Const 3) (Monadic.Var "y")) [("x", Monadic.NumVal 3), ("y", Monadic.NumVal 5)]
+--     putStr "x + 5 = "
+--     print $ Monadic.eval (Monadic.Add (Monadic.Var "x") (Monadic.Const 5)) [("x", Monadic.NumVal 3), ("y", Monadic.NumVal 5)]
+--     putStr "x + y = "
+--     print $ Monadic.eval (Monadic.Add (Monadic.Var "x") (Monadic.Var "y")) [("x", Monadic.NumVal 3), ("y", Monadic.NumVal 5)]
+    
+--     putStrLn "\n-------- Function evaluation: ----------------------------"
+--     let f = Monadic.Fun ["x", "y"] (Monadic.Add (Monadic.Var "x") (Monadic.Var "y")) 
+--     putStr $ "f(x,y) = " ++ show (Monadic.eval f [])
+
+--     putStrLn "now let [(x = 7)]"
+--     putStr $ "f(x,y) = " ++ show (Monadic.eval f [("x", Monadic.NumVal 7)])
+    
+--     putStrLn "\n-------- Function application evaluation: ----------------"
+--     putStr "f(x=1,y=2) = "
+--     print $ Monadic.eval (Monadic.App f [Monadic.Const 1, Monadic.Const 2]) []
+
+--     putStrLn "now let [(x = 7)]"
+--     putStr "f(x=1,y=2) = "
+--     print $ Monadic.eval (Monadic.App f [Monadic.Const 1, Monadic.Const 2]) [("x", Monadic.NumVal 7)]
+
